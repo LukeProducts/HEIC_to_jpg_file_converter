b1f2248a715c7ff8b0c8cf609347407d7e7091d3
LukeProducts <73026669+LukeProducts@users.noreply.github.com>
2020-11-25 10:19:17 +0100

    Add files via upload

---
+++ fileconverter.py
@@ -0,0 +1,79 @@
+import os
+
+Program = True
+Zahl = 1
+
+def bye():
+    print("ok process finished...\nbye!")
+
+def startsymbol():
+    os.system('color b')
+    print("""
+ /$$   /$$ /$$$$$$$$ /$$$$$$  /$$$$$$        /$$$$$$$$ /$$$$$$           /$$$$$ /$$$$$$$   /$$$$$$ 
+| $$  | $$| $$_____/|_  $$_/ /$$__  $$      |__  $$__//$$__  $$         |__  $$| $$__  $$ /$$__  $$
+| $$  | $$| $$        | $$  | $$  \__/         | $$  | $$  \ $$            | $$| $$  \ $$| $$  \__/
+| $$$$$$$$| $$$$$     | $$  | $$               | $$  | $$  | $$            | $$| $$$$$$$/| $$ /$$$$
+| $$__  $$| $$__/     | $$  | $$               | $$  | $$  | $$       /$$  | $$| $$____/ | $$|_  $$
+| $$  | $$| $$        | $$  | $$    $$         | $$  | $$  | $$      | $$  | $$| $$      | $$  \ $$
+| $$  | $$| $$$$$$$$ /$$$$$$|  $$$$$$/         | $$  |  $$$$$$/      |  $$$$$$/| $$      |  $$$$$$/
+|__/  |__/|________/|______/ \______/          |__/   \______/        \______/ |__/       \______/ 
+                                                                                                   
+                                                                                                   
+                                                                                                   
+  /$$$$$$   /$$$$$$  /$$   /$$ /$$    /$$ /$$$$$$$$ /$$$$$$$  /$$$$$$$$ /$$$$$$$$ /$$$$$$$         
+ /$$__  $$ /$$__  $$| $$$ | $$| $$   | $$| $$_____/| $$__  $$|__  $$__/| $$_____/| $$__  $$        
+| $$  \__/| $$  \ $$| $$$$| $$| $$   | $$| $$      | $$  \ $$   | $$   | $$      | $$  \ $$        
+| $$      | $$  | $$| $$ $$ $$|  $$ / $$/| $$$$$   | $$$$$$$/   | $$   | $$$$$   | $$$$$$$/        
+| $$      | $$  | $$| $$  $$$$ \  $$ $$/ | $$__/   | $$__  $$   | $$   | $$__/   | $$__  $$        
+| $$    $$| $$  | $$| $$\  $$$  \  $$$/  | $$      | $$  \ $$   | $$   | $$      | $$  \ $$        
+|  $$$$$$/|  $$$$$$/| $$ \  $$   \  $/   | $$$$$$$$| $$  | $$   | $$   | $$$$$$$$| $$  | $$        
+ \______/  \______/ |__/  \__/    \_/    |________/|__/  |__/   |__/   |________/|__/  |__/         
+    """)
+
+def convert(count):
+    for f in os.listdir():
+        if ".HEIC" in str(f):
+            f = str(f)
+            print("converting...")
+            filename = "Bild_" + str(count) + ".jpg"
+            os.rename(f, filename)
+            count = count + 1
+
+def filefound(f):
+    files_found = '".HEIC"-Files were found!'
+    Anzahl = f.count(".HEIC")
+    print(Anzahl, files_found)
+    Liste = os.listdir(os.getcwd())
+    convert(Zahl)
+
+startsymbol()
+def search():
+    list = os.listdir(os.getcwd())
+    print("Searching:", os.getcwd())
+    if ".HEIC" in str(list):
+        list = str(list)
+        filefound(list)
+    else:
+        print("No .HEIC-Files found in this Directory.\nquitting...")
+
+
+while Program:
+    X = input("Soll das Konvertieren in .jpg gestartet werden?\nJA[ENTER] oder Nein[N]")
+    os.system('color A')
+    if X == '':
+        search()
+    if X == 'N':
+        bye()
+    if X == 'n':
+        bye()
+    else:
+        bye()
+        Program = False
+
+
+
+
+
+
+
+
